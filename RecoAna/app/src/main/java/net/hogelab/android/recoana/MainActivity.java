97d441414fc01027adba5b0c1a8a5997cd9d047e
Hirohisa Kobayasi <koba@hogelab.net>
2021-08-29 22:00:57 +0900

    create simple activity application from template

---
+++ RecoAna/app/src/main/java/net/hogelab/android/recoana/MainActivity.java
@@ -0,0 +1,14 @@
+package net.hogelab.android.recoana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
